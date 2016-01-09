1b48b213c494c35b9e29d0f4f0e6388134655edf
websideas <websideas.corp@gmail.com>
2016-01-09 21:48:42 +0700

    Change

--- wp-content/themes/aquila/templates/content-404.php
+++ wp-content/themes/aquila/templates/content-404.php
@@ -3,7 +3,7 @@
     <div class="page-not-found">
         <h1><?php esc_html_e('404', 'aquila'); ?></h1>
         <h3><?php esc_html_e('Oops, page not found.', 'aquila') ?></h3>
-        <p ><?php wp_kses(esc_html_e('It looks like nothing was found at this location. <br />Click the link below to return home.', 'aquila' ),array( 'br' => array() )); ?></p>
+        <p ><?php echo wp_kses(__('It looks like nothing was found at this location. <br />Click the link below to return home.', 'aquila' ),array( 'br' => array() )); ?></p>
         <div class="buttons">
             <a title="<?php esc_html_e('Back to Home', 'aquila'); ?>" href="<?php echo esc_url( home_url( '/' ) ); ?>">
                 <?php esc_html_e('Return home', 'aquila' ); ?>
