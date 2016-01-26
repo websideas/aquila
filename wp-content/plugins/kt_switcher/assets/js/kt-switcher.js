5291be4ec78516204de277af05a6e6093c4432f8
websideas <websideas.corp@gmail.com>
2016-01-26 23:25:10 +0700

    Update

--- wp-content/plugins/kt_switcher/assets/js/kt-switcher.js
+++ wp-content/plugins/kt_switcher/assets/js/kt-switcher.js
@@ -1,6 +1,87 @@
 (function($){
     "use strict"; // Start of use strict
 
-    console.log('call');
+
+
+    /* ---------------------------------------------
+     Scripts ready
+     --------------------------------------------- */
+    $(document).ready(function() {
+
+        $('body').on('click', '.switcher-toggle', function (e) {
+            e.preventDefault();
+            var $switcher = $('.switcher'),
+                $overlay = $('.demo-overlay'),
+                $html = $('html');
+            if($switcher.hasClass('active')){
+                $switcher.removeClass('active');
+                $overlay.removeClass('active');
+                $html.removeAttr('style');
+            }else{
+                var scrollbarWidth = _getScrollbarSize();
+                $html.css({
+                    overflow: "hidden",
+                    marginLeft: -scrollbarWidth
+                });
+
+                $switcher.addClass('active');
+                $overlay.addClass('active');
+            }
+        }).on('click', '.demo-overlay', function(){
+            var $switcher = $('.switcher'),
+                $overlay = $('.demo-overlay');
+
+            $switcher.removeClass('active');
+            $overlay.removeClass('active');
+        });
+
+        $('#switcher_layout').on('change', function(){
+            var $val = $(this).val(),
+                $_body = $('body');
+            if($val == 'boxed'){
+                $_body.addClass('layout-boxed');
+            }else{
+                $_body.removeClass('layout-boxed');
+                $_body.removeAttr('style');
+            }
+            $(window).trigger("resize");
+        });
+
+        $('#switcher_background').on('click', 'a', function(e){
+            e.preventDefault();
+            var $this = $(this);
+            $('body').css({
+                'background-image': 'url('+$this.find('img').attr('src')+')',
+                'background-repeat': $this.data('repeat')
+            });
+        });
+
+        $('#switcher_color').on('click', 'a', function(e){
+            e.preventDefault();
+
+            var skin_url =  $(this).attr('href');
+            $('#kt-main-inline-css').remove();
+
+            if( $('link#ds-skin').length > 0 ){
+                $('link#ds-skin').attr( 'href', skin_url );
+            }else{
+                $('head').append("<link id='ds-skin' rel='stylesheet' href='"+skin_url+"' type='text/css' media='all' />");
+            }
+
+
+        });
+
+    });
+
+
+    function _getScrollbarSize() {
+        var scrollDiv = document.createElement("div"),
+            scrollbarSize;
+        scrollDiv.style.cssText = 'width: 99px; height: 99px; overflow: scroll; position: absolute; top: -9999px;';
+        document.body.appendChild(scrollDiv);
+        scrollbarSize = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+        document.body.removeChild(scrollDiv);
+        return scrollbarSize;
+    }
 
 })(jQuery);
--- wp-content/plugins/kt_switcher/kt_switcher.php
+++ wp-content/plugins/kt_switcher/kt_switcher.php
@@ -14,6 +14,8 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
 class KT_SWITCHER
 {
 
+    var $image_url;
+
     /**
      * Start up
      */
@@ -21,6 +23,7 @@ class KT_SWITCHER
     {
         add_action( 'wp_enqueue_scripts', array( $this, 'kt_switcher_scripts' ));
         add_action( 'wp_footer', array( $this, 'kt_switcher_footer' ));
+        $this->assets = plugins_url( '/assets/', __FILE__);
     }
     /**
      * Add scripts
@@ -28,65 +31,86 @@ class KT_SWITCHER
      *
      */
     function kt_switcher_scripts(){
-        wp_enqueue_style( 'kt-switcher-css', plugins_url( '/assets/css/kt-switcher.css', __FILE__ ) );
-        wp_enqueue_script( 'kt-switcher-js', plugins_url( '/assets/js/kt-switcher.js', __FILE__ ), array( 'jquery' ) );
+        wp_enqueue_style( 'kt-switcher', plugins_url( '/assets/css/kt-switcher.css', __FILE__ ) );
+        wp_enqueue_script( 'kt-switcher', plugins_url( '/assets/js/kt-switcher.js', __FILE__ ), array( 'jquery' ) );
     }
 
     function kt_switcher_footer(){
 
-        $home_url = get_home_url();
 
         ?>
-
+        <div class="demo-overlay"></div>
         <div class="switcher">
-            <a href="#" class="switcher-toggle"><i class="fa fa-cogs"></i></a>
+            <a href="#" class="switcher-toggle"><i class="fa fa-cog fa-spin"></i></a>
 
-            <div class="switcher-head">
-                <a title="Buy Theme Now" href="#" class="button btn-buy">Buy Theme Now</a>
-            </div>
+            <div class="switcher-inner">
 
-            Reset
-
-            <div class="switcher-panel">
-                <div class="switcher-heading">Color examples</div>
-
-                <div class="form-group">
-                    Demo versions
+                <div class="switcher-head">
+                    Style Selector
                 </div>
 
-                <div class="form-group">
-                    <label>Theme Color</label>
-                    <div id="switcher_color">
-                        <a data-color="c4b498" href="#" style="background-color: rgb(196, 180, 152);">#c4b498</a>
-                        <a data-color="e34444" href="#" class="active color" style="background-color: rgb(227, 68, 68);">#e34444</a>
-                        <a data-color="17a7f1" href="#" style="background-color: rgb(23, 167, 241);">#17a7f1</a>
-                        <a data-color="2fc961" href="#" style="background-color: rgb(47, 201, 97);">#2fc961</a>
-                        <a data-color="5e36e1" href="#" style="background-color: rgb(94, 54, 225);">#5e36e1</a>
-                        <a data-color="f3bb25" href="#" style="background-color: rgb(243, 187, 37);">#f3bb25</a>
+
+                <div class="switcher-panel">
+
+                    <div class="form-group">
+                        <label>Theme Color</label>
+                        <div id="switcher_color" class="clearfix">
+                            <a data-color="c4b498" href="<?php echo $this->assets; ?>skins/color-6dab3c.css" style="background-color: #6dab3c;">#6dab3c</a>
+                            <a data-color="e34444" href="<?php echo $this->assets; ?>skins/color-e34444.css" style="background-color: #e34444">#e34444</a>
+                            <a data-color="17a7f1" href="<?php echo $this->assets; ?>skins/color-17a7f1.css" style="background-color: #17a7f1;">#17a7f1</a>
+                            <a data-color="2fc961" href="<?php echo $this->assets; ?>skins/color-2fc961.css" style="background-color: #2fc961;">#2fc961</a>
+                            <a data-color="5e36e1" href="<?php echo $this->assets; ?>skins/color-5e36e1.css" style="background-color: #5e36e1;">#5e36e1</a>
+                            <a data-color="f3bb25" href="<?php echo $this->assets; ?>skins/color-f3bb25.css" style="background-color: #f3bb25;">#f3bb25</a>
+                            <a data-color="17a7f1" href="<?php echo $this->assets; ?>skins/color-17a7f1.css" style="background-color: #17a7f1;">#17a7f1</a>
+                            <a data-color="2fc961" href="<?php echo $this->assets; ?>skins/color-2fc961.css" style="background-color: #2fc961;">#2fc961</a>
+                        </div>
+                        <p>You can also sellect color codes via admin theme options</p>
                     </div>
-                    <p>You can also sellect color codes via admin theme options</p>
-                </div>
-                <div class="form-group">
-                    <label for="switcher_layout">Layout options</label>
-                    <select name="layout" id="switcher_layout">
-                        <option value="wide">Full Width</option>
-                        <option value="boxed">Boxed Mod</option>
-                    </select>
-                </div>
-                <div>
-                    <label>Boxed background examples</label>
-                    <div id="switcher_background">
-                        <a href="#" class="active"></a>
+                    <div class="form-group">
+                        <label for="switcher_layout">Layout options</label>
+                        <select name="layout" id="switcher_layout" autocomplete="off">
+                            <option value="wide" selected="selected">Full Width</option>
+                            <option value="boxed">Boxed Mod</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label>Boxed background examples</label>
+                        <div id="switcher_background">
+                            <a href="#" data-repeat="repeat"><img src="<?php echo $this->assets; ?>patterns/congruent_pentagon.png" alt=""/></a>
+                            <a href="#" data-repeat="no-repeat"><img src="<?php echo $this->assets; ?>patterns/criss-xcross.png" alt=""/></a>
+                            <a href="#" data-repeat="repeat"><img src="<?php echo $this->assets; ?>patterns/criss-xcross.png" alt=""/></a>
+                            <a href="#" data-repeat="repeat"><img src="<?php echo $this->assets; ?>patterns/criss-xcross.png" alt=""/></a>
+                            <a href="#" data-repeat="repeat"><img src="<?php echo $this->assets; ?>patterns/criss-xcross.png" alt=""/></a>
+                        </div>
                     </div>
                 </div>
 
-                Body font
-                Source Sans Pro
 
-                Header font
-                Source Sans Pro
+                <div class="switcher-footer">
+                    <label>See other demos</label>
+                    <div id="other_demos" class="clearfix">
+                        <div class="demo-item">
+                            <a href="#"><img src="<?php echo $this->assets; ?>patterns/congruent_pentagon.png" alt=""/> <span>View</span></a>
+                        </div>
+                        <div class="demo-item">
+                            <a href="#"><img src="<?php echo $this->assets; ?>patterns/criss-xcross.png" alt=""/> <span>View</span></a>
+                        </div>
+                        <div class="demo-item">
+                            <a href="#"><img src="<?php echo $this->assets; ?>patterns/criss-xcross.png" alt=""/> <span>View</span></a>
+                        </div>
+                        <div class="demo-item">
+                            <a href="#"><img src="<?php echo $this->assets; ?>patterns/criss-xcross.png" alt=""/> <span>View</span></a>
+                        </div>
+                        <div class="demo-item">
+                            <a href="#"><img src="<?php echo $this->assets; ?>patterns/criss-xcross.png" alt=""/> <span>View</span></a>
+                        </div>
+                        <div class="demo-item">
+                            <a href="#"><img src="<?php echo $this->assets; ?>patterns/criss-xcross.png" alt=""/> <span>View</span></a>
+                        </div>
+                    </div>
+                </div>
+                </div>
 
-            </div>
         </div>
         <?php
     }
